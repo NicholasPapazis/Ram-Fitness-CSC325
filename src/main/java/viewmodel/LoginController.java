529a1473b861621bc8b0c308fdd4a7ac81847562
Nicholas Papazis <nicholasepapazis@gmail.com>
2024-11-08 17:31:24 -0500

    Added login controller

--- src/main/java/viewmodel/LoginController.java
+++ src/main/java/viewmodel/LoginController.java
@@ -1,4 +1,46 @@
 package viewmodel;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 public class LoginController {
+
+    @FXML
+    private Text signUpLink;
+
+    //changes scene to show sign up page
+    public void signUp(MouseEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/signUp.fxml"));
+            Scene scene = new Scene(root, 900, 600);
+            scene.getStylesheets().add(getClass().getResource("/css/signUp.css").toExternalForm());
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //changes scene to show dashboard page
+    public void dashboard(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/dashboard.fxml"));
+            Scene scene = new Scene(root, 900, 600);
+            scene.getStylesheets().add(getClass().getResource("/css/dashboard.css").toExternalForm());
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
