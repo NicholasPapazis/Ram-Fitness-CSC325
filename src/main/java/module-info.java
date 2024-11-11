5ed7729517da53992561024e4fcf3c1c1e9e168e
NicholasPapazis <nicholasepapazis@gmail.com>
2024-11-11 17:02:26 -0500

    fixed dashboard controller. Everything works as expected, splash, login, signup, dashboard.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.ramfitnesscsc325 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     /*opens com.example.ramfitnesscsc325 to javafx.fxml;
