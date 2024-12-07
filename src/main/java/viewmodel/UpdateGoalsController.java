5a24873a121c985546ee58478a028a84eeccbc97
Nicholas Papazis <nicholasepapazis@gmail.com>
2024-12-07 14:55:13 -0500

    complete status bar functionality for updateData.fxml

--- src/main/java/viewmodel/UpdateGoalsController.java
+++ src/main/java/viewmodel/UpdateGoalsController.java
@@ -8,9 +8,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -61,8 +63,51 @@ public class UpdateGoalsController {
 
     @FXML
     private Pane dashboardLink;
+    @FXML
+    private Pane updateGoalsLink;
+    @FXML
+    private Pane getStartedLink;
+    @FXML
+    private Pane settingsLink;
+
+    @FXML
+    private Pane helpLink;
+    @FXML
+    private Pane logoutLink;
+
+    @FXML
+    private Button saveBtn;
 
 
+    @FXML
+    private Text statusText;
+
+
+    public void initialize() {
+
+        //hover effects for each pane on side bar
+        setupHoverEffect(dashboardLink, "click to go to Dashboard");
+        setupHoverEffect(updateGoalsLink, "click to update your goals");
+        setupHoverEffect(getStartedLink, "click to learn about working out");
+        setupHoverEffect(settingsLink, "click to open settings");
+
+        setupHoverEffect(helpLink, "click for instructions");
+        setupHoverEffect(logoutLink, "click logout of your account");
+
+        setupHoverEffect(saveBtn, "click to save changes");
+
+        setupHoverEffect(exerciseProgress, "modify your daily exercise time");
+        setupHoverEffect(caloriesProgress, "modify your daily calorie intake");
+        setupHoverEffect(weightProgress, "modify your daily weight lifted total");
+        setupHoverEffect(sleepProgress, "modify the amount of sleep you received last night");
+
+        setupHoverEffect(exerciseGoal, "modify your daily exercise time goal");
+        setupHoverEffect(caloriesGoal, "modify your daily calorie intake goal");
+        setupHoverEffect(weightGoal, "modify your daily weight lifted total goal");
+        setupHoverEffect(sleepGoal, "modify the amount of sleep you received last night goal");
+
+
+    }
 
     /****** sidebar methods start *******/
 
@@ -181,6 +226,40 @@ public class UpdateGoalsController {
         }
     }
 
+
+
+
+    /* status bar methods start */
+
+    //detects where mouse is for pane
+    private void setupHoverEffect(Pane pane, String message){
+        pane.setOnMouseEntered(event -> showStatusMessage(message));
+        pane.setOnMouseExited(event -> clearStatusMessage());
+    }
+
+    //detects where mouse is for button
+    private void setupHoverEffect(Button btn, String message){
+        btn.setOnMouseEntered(event -> showStatusMessage(message));
+        btn.setOnMouseExited(event -> clearStatusMessage());
+    }
+
+    //detects where mouse is for button
+    private void setupHoverEffect(TextField txt, String message){
+        txt.setOnMouseEntered(event -> showStatusMessage(message));
+        txt.setOnMouseExited(event -> clearStatusMessage());
+    }
+
+    //shows status message
+    private void showStatusMessage(String message) {
+        statusText.setText(message);
+    }
+
+    //clears status message
+    private void clearStatusMessage() {
+        statusText.setText(""); // Clear the message when mouse leaves the Pane
+    }
+
+    /* status bar methods end */
 
 
 
