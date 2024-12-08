44c1fd0bd22839ae6412ddd2f3f4f4074f4d3daf
Nicholas Papazis <nicholasepapazis@gmail.com>
2024-12-07 23:43:07 -0500

    added regex validation to Update Goals page so that user can only input numerical values

--- src/main/java/viewmodel/UpdateGoalsController.java
+++ src/main/java/viewmodel/UpdateGoalsController.java
@@ -19,6 +19,8 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UpdateGoalsController {
 
@@ -83,6 +85,26 @@ public class UpdateGoalsController {
     private Text statusText;
 
 
+    @FXML
+    private Text alertTextExerciseProgress;
+    @FXML
+    private Text alertTextCaloriesProgress;
+    @FXML
+    private Text alertTextWeightProgress;
+    @FXML
+    private Text alertTextSleepProgress;
+
+    @FXML
+    private Text alertTextExerciseGoal;
+    @FXML
+    private Text alertTextCaloriesGoal;
+    @FXML
+    private Text alertTextWeightGoal;
+    @FXML
+    private Text alertTextSleepGoal;
+
+
+
     public void initialize() {
 
         //hover effects for each pane on side bar
@@ -105,6 +127,43 @@ public class UpdateGoalsController {
         setupHoverEffect(caloriesGoal, "modify your daily calorie intake goal");
         setupHoverEffect(weightGoal, "modify your daily weight lifted total goal");
         setupHoverEffect(sleepGoal, "modify the amount of sleep you received last night goal");
+
+
+
+
+        exerciseProgress.textProperty().addListener((observable, oldValue, newValue) -> {
+            validateExerciseProgress();
+        });
+        caloriesProgress.textProperty().addListener((observable, oldValue, newValue) -> {
+            validateCaloriesProgress();
+        });
+        weightProgress.textProperty().addListener((observable, oldValue, newValue) -> {
+            validateWeightProgress();
+        });
+        sleepProgress.textProperty().addListener((observable, oldValue, newValue) -> {
+            validateSleepProgress();
+        });
+
+        exerciseGoal.textProperty().addListener((observable, oldValue, newValue) -> {
+            validateExerciseGoal();
+        });
+        caloriesGoal.textProperty().addListener((observable, oldValue, newValue) -> {
+            validateCaloriesGoal();
+        });
+        weightGoal.textProperty().addListener((observable, oldValue, newValue) -> {
+            validateWeightGoal();
+        });
+        sleepGoal.textProperty().addListener((observable, oldValue, newValue) -> {
+            validateSleepGoal();
+        });
+
+        exerciseProgress.textProperty().addListener((observable, oldValue, newValue) -> checkValidationStatus());
+        caloriesProgress.textProperty().addListener((observable, oldValue, newValue) -> checkValidationStatus());
+        weightProgress.textProperty().addListener((observable, oldValue, newValue) -> checkValidationStatus());
+        sleepProgress.textProperty().addListener((observable, oldValue, newValue) -> checkValidationStatus());
+
+
+
 
 
     }
@@ -262,6 +321,268 @@ public class UpdateGoalsController {
     /* status bar methods end */
 
 
+
+    /* validate fields start */
+
+    // Validation for exerciseProgress
+    private boolean validateExerciseProgress() {
+        final String regex = "\\d+"; //ensure only digits
+        String userInput = exerciseProgress.getText(); //gets text from input
+
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(userInput);
+
+        boolean inputValid = matcher.matches(); //check if input matches regex
+
+        //checks if above statement is valid
+        if(inputValid){ //if input is valid
+            exerciseProgress.setStyle("-fx-border-color: #15bc98"); //sets border color of input field to green
+            alertTextExerciseProgress.setText(""); //gets rid of any alert text in alert box
+            return true; //returns true because the input is valid
+        }
+        else if(exerciseProgress.isFocused() && !userInput.isEmpty()  ) { //if field is focused and not empty
+            exerciseProgress.setStyle("-fx-border-color: red");
+            alertTextExerciseProgress.setStyle("-fx-text-fill: red");
+            alertTextExerciseProgress.setStyle("-fx-font-size: 10px");
+            alertTextExerciseProgress.setText("* must be a numerical value");
+
+        } else { //if the field is empty or not focused, then remove styling and alert
+            exerciseProgress.setStyle("");
+            alertTextExerciseProgress.setText("");
+        }
+        return false; //return false if user has not yet inputed anything into the text field
+
+    }
+
+    // Validation for caloriesProgress
+    private boolean validateCaloriesProgress() {
+        final String regex = "\\d+"; //ensure only digits
+        String userInput = caloriesProgress.getText(); //gets text from input
+
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(userInput);
+
+        boolean inputValid = matcher.matches(); //check if input matches regex
+
+        //checks if above statement is valid
+        if(inputValid){ //if input is valid
+            caloriesProgress.setStyle("-fx-border-color: #15bc98"); //sets border color of input field to green
+            alertTextCaloriesProgress.setText(""); //gets rid of any alert text in alert box
+            return true; //returns true because the input is valid
+        }
+        else if(caloriesProgress.isFocused() && !userInput.isEmpty()  ) { //if field is focused and not empty
+            caloriesProgress.setStyle("-fx-border-color: red");
+            alertTextCaloriesProgress.setStyle("-fx-text-fill: red");
+            alertTextCaloriesProgress.setStyle("-fx-font-size: 10px");
+            alertTextCaloriesProgress.setText("* must be a numerical value");
+
+        } else { //if the field is empty or not focused, then remove styling and alert
+            caloriesProgress.setStyle("");
+            alertTextCaloriesProgress.setText("");
+        }
+        return false; //return false if user has not yet inputed anything into the text field
+
+    }
+
+
+    // Validation for weightProgress
+    private boolean validateWeightProgress() {
+        final String regex = "\\d+"; //ensure only digits
+        String userInput = weightProgress.getText(); //gets text from input
+
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(userInput);
+
+        boolean inputValid = matcher.matches(); //check if input matches regex
+
+        //checks if above statement is valid
+        if(inputValid){ //if input is valid
+            weightProgress.setStyle("-fx-border-color: #15bc98"); //sets border color of input field to green
+            alertTextWeightProgress.setText(""); //gets rid of any alert text in alert box
+            return true; //returns true because the input is valid
+        }
+        else if(weightProgress.isFocused() && !userInput.isEmpty()  ) { //if field is focused and not empty
+            weightProgress.setStyle("-fx-border-color: red");
+            alertTextWeightProgress.setStyle("-fx-text-fill: red");
+            alertTextWeightProgress.setStyle("-fx-font-size: 10px");
+            alertTextWeightProgress.setText("* must be a numerical value");
+
+        } else { //if the field is empty or not focused, then remove styling and alert
+            weightProgress.setStyle("");
+            alertTextWeightProgress.setText("");
+        }
+        return false; //return false if user has not yet inputted anything into the text field
+
+    }
+
+    // Validation for sleepProgress
+    private boolean validateSleepProgress() {
+        final String regex = "\\d+"; //ensure only digits
+        String userInput = sleepProgress.getText(); //gets text from input
+
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(userInput);
+
+        boolean inputValid = matcher.matches(); //check if input matches regex
+
+        //checks if above statement is valid
+        if(inputValid){ //if input is valid
+            sleepProgress.setStyle("-fx-border-color: #15bc98"); //sets border color of input field to green
+            alertTextSleepProgress.setText(""); //gets rid of any alert text in alert box
+            return true; //returns true because the input is valid
+        }
+        else if(sleepProgress.isFocused() && !userInput.isEmpty()  ) { //if field is focused and not empty
+            sleepProgress.setStyle("-fx-border-color: red");
+            alertTextSleepProgress.setStyle("-fx-text-fill: red");
+            alertTextSleepProgress.setStyle("-fx-font-size: 10px");
+            alertTextSleepProgress.setText("* must be a numerical value");
+
+        } else { //if the field is empty or not focused, then remove styling and alert
+            sleepProgress.setStyle("");
+            alertTextSleepProgress.setText("");
+        }
+        return false; //return false if user has not yet inputted anything into the text field
+
+    }
+
+
+    //goal fields below
+
+    // Validation for exerciseGoal
+    private boolean validateExerciseGoal() {
+        final String regex = "\\d+"; //ensure only digits
+        String userInput = exerciseGoal.getText(); //gets text from input
+
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(userInput);
+
+        boolean inputValid = matcher.matches(); //check if input matches regex
+
+        //checks if above statement is valid
+        if(inputValid){ //if input is valid
+            exerciseGoal.setStyle("-fx-border-color: #15bc98"); //sets border color of input field to green
+            alertTextExerciseGoal.setText(""); //gets rid of any alert text in alert box
+            return true; //returns true because the input is valid
+        }
+        else if(exerciseGoal.isFocused() && !userInput.isEmpty()  ) { //if field is focused and not empty
+            exerciseGoal.setStyle("-fx-border-color: red");
+            alertTextExerciseGoal.setStyle("-fx-text-fill: red");
+            alertTextExerciseGoal.setStyle("-fx-font-size: 10px");
+            alertTextExerciseGoal.setText("* must be a numerical value");
+
+        } else { //if the field is empty or not focused, then remove styling and alert
+            exerciseGoal.setStyle("");
+            alertTextExerciseGoal.setText("");
+        }
+        return false; //return false if user has not yet inputed anything into the text field
+
+    }
+
+    // Validation for caloriesProgress
+    private boolean validateCaloriesGoal() {
+        final String regex = "\\d+"; //ensure only digits
+        String userInput = caloriesGoal.getText(); //gets text from input
+
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(userInput);
+
+        boolean inputValid = matcher.matches(); //check if input matches regex
+
+        //checks if above statement is valid
+        if(inputValid){ //if input is valid
+            caloriesGoal.setStyle("-fx-border-color: #15bc98"); //sets border color of input field to green
+            alertTextCaloriesGoal.setText(""); //gets rid of any alert text in alert box
+            return true; //returns true because the input is valid
+        }
+        else if(caloriesGoal.isFocused() && !userInput.isEmpty()  ) { //if field is focused and not empty
+            caloriesGoal.setStyle("-fx-border-color: red");
+            alertTextCaloriesGoal.setStyle("-fx-text-fill: red");
+            alertTextCaloriesGoal.setStyle("-fx-font-size: 10px");
+            alertTextCaloriesGoal.setText("* must be a numerical value");
+
+        } else { //if the field is empty or not focused, then remove styling and alert
+            caloriesGoal.setStyle("");
+            alertTextCaloriesGoal.setText("");
+        }
+        return false; //return false if user has not yet inputed anything into the text field
+
+    }
+
+
+    // Validation for weightProgress
+    private boolean validateWeightGoal() {
+        final String regex = "\\d+"; //ensure only digits
+        String userInput = weightGoal.getText(); //gets text from input
+
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(userInput);
+
+        boolean inputValid = matcher.matches(); //check if input matches regex
+
+        //checks if above statement is valid
+        if(inputValid){ //if input is valid
+            weightGoal.setStyle("-fx-border-color: #15bc98"); //sets border color of input field to green
+            alertTextWeightGoal.setText(""); //gets rid of any alert text in alert box
+            return true; //returns true because the input is valid
+        }
+        else if(weightGoal.isFocused() && !userInput.isEmpty()  ) { //if field is focused and not empty
+            weightGoal.setStyle("-fx-border-color: red");
+            alertTextWeightGoal.setStyle("-fx-text-fill: red");
+            alertTextWeightGoal.setStyle("-fx-font-size: 10px");
+            alertTextWeightGoal.setText("* must be a numerical value");
+
+        } else { //if the field is empty or not focused, then remove styling and alert
+            weightGoal.setStyle("");
+            alertTextWeightGoal.setText("");
+        }
+        return false; //return false if user has not yet inputted anything into the text field
+
+    }
+
+    // Validation for sleepProgress
+    private boolean validateSleepGoal() {
+        final String regex = "\\d+"; //ensure only digits
+        String userInput = sleepGoal.getText(); //gets text from input
+
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(userInput);
+
+        boolean inputValid = matcher.matches(); //check if input matches regex
+
+        //checks if above statement is valid
+        if(inputValid){ //if input is valid
+            sleepGoal.setStyle("-fx-border-color: #15bc98"); //sets border color of input field to green
+            alertTextSleepGoal.setText(""); //gets rid of any alert text in alert box
+            return true; //returns true because the input is valid
+        }
+        else if(sleepGoal.isFocused() && !userInput.isEmpty()  ) { //if field is focused and not empty
+            sleepGoal.setStyle("-fx-border-color: red");
+            alertTextSleepGoal.setStyle("-fx-text-fill: red");
+            alertTextSleepGoal.setStyle("-fx-font-size: 10px");
+            alertTextSleepGoal.setText("* must be a numerical value");
+
+        } else { //if the field is empty or not focused, then remove styling and alert
+            sleepGoal.setStyle("");
+            alertTextSleepGoal.setText("");
+        }
+        return false; //return false if user has not yet inputted anything into the text field
+
+    }
+
+
+
+
+
+    //validation methods:
+    private void checkValidationStatus() {
+        //check if all fields are valid
+        boolean allFieldsValid = validateExerciseProgress() && validateCaloriesProgress() && validateWeightProgress() && validateSleepProgress() && validateExerciseGoal() && validateCaloriesGoal() && validateWeightGoal() && validateSleepGoal();
+
+        //enable register button if all fields are valid
+        saveBtn.setDisable(!allFieldsValid);
+    }
+
+    /* validate fields end */
 
 
 
